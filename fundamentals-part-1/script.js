9990656455f2a878296a1bc11a1f9673d7f8f741
Samir Jadhav <samirzjadhav@gmail.com>
2022-07-14 17:58:45 +0530

    feat: add some variable and see how it work

--- fundamentals-part-1/script.js
+++ fundamentals-part-1/script.js
@@ -1,7 +1,32 @@
 // ALERT SECTION
 let js = "amazing";
-if (js === "amazing") alert("javascript is FUN!");
+// if (js === "amazing") alert("javascript is FUN!");
 
 // CONSOLE.LOG
-
 console.log(34 + 45 + 5 - 13);
+console.log("samir");
+console.log(18);
+
+let firstName = "samir";
+let middleName = "zaduji";
+let lastName = "jadhav";
+
+console.log(firstName + " " + middleName + " " + lastName);
+console.log(lastName + " " + firstName + " " + middleName);
+
+/**
+ * using array.prototype.join to join using spaces
+ */
+// console.log([firstName, middleName, lastName].join(" "));
+// console.log([firstName, middleName, lastName].reverse().join(" "));
+
+/**
+ * template strings
+ */
+// console.log(`${firstName} ${middleName} ${lastName}`);
+
+let myFirstJob = "programmer";
+let myCurrentJob = "teacher";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
