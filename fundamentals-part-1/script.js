d0b075e2e1914225406f26ffcc1a897cf9bc5e9a
Samir Jadhav <samirzjadhav@gmail.com>
2022-07-15 22:37:34 +0530

    feat: add operator

--- fundamentals-part-1/script.js
+++ fundamentals-part-1/script.js
@@ -1,11 +1,16 @@
+/*
 // ALERT SECTION
-let js = "amazing";
-// if (js === "amazing") alert("javascript is FUN!");
 
-// CONSOLE.LOG
+let js = "amazing";
+if (js === "amazing") alert("javascript is FUN!");
+
+// STRING AND NUMBER 
+
 console.log(34 + 45 + 5 - 13);
 console.log("samir");
 console.log(18);
+
+// VARIABLE NAME CONVENSTIONS
 
 let firstName = "samir";
 let middleName = "zaduji";
@@ -14,19 +19,106 @@ let lastName = "jadhav";
 console.log(firstName + " " + middleName + " " + lastName);
 console.log(lastName + " " + firstName + " " + middleName);
 
-/**
- * using array.prototype.join to join using spaces
- */
-// console.log([firstName, middleName, lastName].join(" "));
-// console.log([firstName, middleName, lastName].reverse().join(" "));
+// using array.prototype.join to join using spaces
+ console.log([firstName, middleName, lastName].join(" "));
+ console.log([firstName, middleName, lastName].reverse().join(" "));
 
-/**
- * template strings
- */
-// console.log(`${firstName} ${middleName} ${lastName}`);
+//  template strings
+ console.log(`${firstName} ${middleName} ${lastName}`);
 
 let myFirstJob = "programmer";
 let myCurrentJob = "teacher";
 
+let job1 = "programmer";
+let job2 = "teacher";
+
 console.log(myFirstJob);
 console.log(myCurrentJob);
+
+let person = "jonas";
+let PI = 23.435;
+*/
+
+/*
+// BOOLEAN
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+ console.log(typeof true);
+console.log(typeof javascriptIsFun);
+ console.log(typeof "jonas");
+ console.log(typeof false);
+ console.log(typeof 23);
+
+// THIS IS DYNAMIC TYPING 
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+*/
+
+/*
+// UNDEFINED
+let year;
+console.log(year);
+console.log(typeof year);
+
+// DYNAMIC TYPING
+year = 2004;
+console.log(typeof year);
+
+// NULL
+console.log(typeof null);
+
+// LET
+// let are use in changing variable and not constant.
+
+let age = 30;
+age = 31;
+
+// CONST
+// const are use in constant variable are the variable not change.
+
+const bithYear = 2004;
+brthYear = 2003;
+
+// NO DECLERATION
+firstName = "samir";
+console.log(firstName);
+*/
+
+/*
+// MATHEMATICALL AND ARITHAMATIC OPERATORS
+// MATH OPERATOR
+const now = 2050;
+const ageSamir = now - 2004; // 2050 - 2004 = 46;
+const ageCuties = now - 2030; // 2050 - 2030 = 20;
+
+console.log(ageSamir, ageCuties);
+console.log(ageSamir * 2, ageSamir / 2, 3 ** 2); // 46 + 46 = 92; // 46 / 2 = 23 ;  // 3 x 3 = 9;
+// 3 ** 2 mean 3 to the power of 2 = 3 * 3 //
+
+// ASSIGNMENT OPERATOR
+let x = 10 + 5; // 17 ;
+x += 10; // 15 + 10 = 25;
+x *= 4; // 42 x 4 = 100;
+x++;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageSamir > ageCuties);
+console.log(ageSamir < ageCuties);
+
+console.log(ageSamir < x);
+console.log(ageSamir >= 20);
+
+const myFullAge = ageSamir >= 20
+
+console.log(now - 2004 > now - 2020)
+
+*/
+
+const now = 2050;
+const ageSamir = now - 2004; // 2050 - 2004 = 46;
+const ageCuties = now - 2030; // 2050 - 2030 = 20;
+
+console.log(now - 2004 > now - 2020);
