4247215f9fbf3a5986951414c5b05ae4ad933473
Samir Jadhav <samirzjadhav@gmail.com>
2022-07-17 21:13:09 +0530

    complete second challenge in fundamental part 1

--- fundamentals-part-1/script.js
+++ fundamentals-part-1/script.js
@@ -218,6 +218,7 @@ console.log(`string with
  lines`);
 */
 
+/*
 //IF AND ELSE CONTROLS STRACTURE
 const age = 18;
 if (age >= 18) {
@@ -235,3 +236,32 @@ if (birthYear <= 2000) {
   century = 21;
 }
 console.log(century);
+*/
+
+/*
+Coding Challenge #2
+
+Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+
+Your tasks:
+
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+
+Hint: Use an if/else statement 😉
+
+GOOD LUCK 😀
+*/
+
+const BMImark = 28.3;
+const BMIjohn = 23.9;
+
+if (BMImark < BMIjohn) {
+  console.log(`mark's BMI (${BMImark}) is heigher than john's(${BMIjohn})!`);
+} else {
+  console.log(`jonh's BMI(${BMIjohn}) is lower than Mark's(${BMImark}) !`);
+}
