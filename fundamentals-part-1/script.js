e5bdcf056e7ba337e248017dcfffb3608d3949b4
Samir Jadhav <samirzjadhav@gmail.com>
2022-07-19 19:35:52 +0530

    complete course challenge 3

--- fundamentals-part-1/script.js
+++ fundamentals-part-1/script.js
@@ -377,3 +377,66 @@ if (hasDriverLicense && hasGoodVision && !isTired) {
   console.log("someone else should drive...");
 }
 */
+
+/*
+Coding Challenge #3
+There are two gymnastics teams, Dolphins and Koalas. They compete against each
+other 3 times. The winner with the highest average score wins a trophy!
+
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks 😉
+
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
+points. Otherwise, no team wins the trophy
+
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+GOOD LUCK 😀
+*/
+
+/*
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas) {
+  console.log("Dolphins win the trophy 🏆");
+} else if (scoreKoalas > scoreDolphins) {
+  console.log("Koalos win the trophy 🏆");
+} else if (scoreKoalas === scoreDolphins) {
+  console.log("both win the trophy 🏆");
+}
+*/
+
+//BONUS 1
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log("Dolphins win the trophy 🏆");
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+  console.log("Koalos win the trophy 🏆");
+} else if (
+  scoreKoalas === scoreDolphins &&
+  scoreDolphins >= 100 &&
+  scoreKoalas >= 100
+) {
+  console.log("both win the trophy 🏆");
+} else {
+  console.log("no one wins the trophy 😭");
+}
