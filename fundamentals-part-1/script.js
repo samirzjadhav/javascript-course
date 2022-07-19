bd780d5863161751b848e424b6e519e742f118dd
Samir Jadhav <samirzjadhav@gmail.com>
2022-07-19 21:04:18 +0530

    conditional (ternary) operator

--- fundamentals-part-1/script.js
+++ fundamentals-part-1/script.js
@@ -441,6 +441,8 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
 }
 */
 
+/*
+//SWITCH STATEMENT
 const day = "monday";
 
 switch (day) {
@@ -480,3 +482,26 @@ if (day === "monday") {
 } else {
   console.log("Not a valid day!");
 }
+*/
+
+/*
+//CONDITIONAL (TERNARY) OPERATOR
+const age = 17;
+age >= 18
+  ? console.log("I like to drink wine 🍷")
+  : console.log("I like to drink water 💧");
+
+const drink = age >= 18 ? "wine 🍷" : "water 💧";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine 🍷";
+} else {
+  drink2 = "water 💧";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`);
+
+*/
