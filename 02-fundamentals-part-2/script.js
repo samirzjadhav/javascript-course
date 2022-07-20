43443a9718aede8924ecec3af3eed7c46bf67e32
Samir Jadhav <samirzjadhav@gmail.com>
2022-07-20 21:21:06 +0530

    misc: activeting strict mode

---
+++ 02-fundamentals-part-2/script.js
@@ -0,0 +1,15 @@
+/*
+// ACTIVETING STRICT MODE
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can Drive :D");
+
+// const interface = "audio";
+// const private = 324;
+
+// console.log(interface, private);
+*/
