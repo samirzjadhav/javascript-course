73c8352a7a7eed3b66543ce5f444856f7a153325
Samir Jadhav <samirzjadhav@gmail.com>
2022-07-21 13:21:48 +0530

    misc: function

--- 02-fundamentals-part-2/script.js
+++ 02-fundamentals-part-2/script.js
@@ -10,6 +10,31 @@ if (hasDriversLicense) console.log("I can Drive :D");
 
 // const interface = "audio";
 // const private = 324;
+//const if = 234;
 
 // console.log(interface, private);
+*/
+
+/*
+//FUNCTION
+function logger() {
+  console.log("My name is Samir");
+}
+
+// CALLING / RUNNING / INVOKKING FUNCTION
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 1);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
 */
