2a109d75239ed103b88661492f6059a0fee91c07
Samir Jadhav <samirzjadhav@gmail.com>
2022-07-25 16:16:10 +0530

    misc: using for loop in ages

--- 02-fundamentals-part-2/script.js
+++ 02-fundamentals-part-2/script.js
@@ -477,3 +477,11 @@ for (let i = 0; i < samir.length; i++) {
   types.push(typeof samir[i]);
 }
 console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2037 - years[i]);
+}
+console.log(ages);
