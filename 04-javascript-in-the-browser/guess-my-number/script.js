7f39c8fff547f9e06318ffc386f3d75d0fee17e6
Samir Jadhav <samirzjadhav@gmail.com>
2022-07-27 21:34:55 +0530

    feat: selecting and manipulating element

--- 04-javascript-in-the-browser/guess-my-number/script.js
+++ 04-javascript-in-the-browser/guess-my-number/script.js
@@ -1,1 +1,8 @@
-'use strict';
+"use strict";
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "🎉 Correct Number!";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 12;
+
+document.querySelector(".guess").value = 19;
