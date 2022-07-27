09695e2c94697f4dac5d43eadc77c7e672ba030c
Samir Jadhav <samirzjadhav@gmail.com>
2022-07-27 11:36:27 +0530

    misc: solved problem no 2

--- 03-developer-skill/script.js
+++ 03-developer-skill/script.js
@@ -1,7 +1,7 @@
 //Remember,we're gonna use strict mode in all scripts now!
 "use script";
 
-//PROBLEM
+//PROBLEM 1
 // We work for a company building a smart home thermometer.Our most recent task is this:
 // "Given an array of temperatures of one day, calculate the temperature amplitude.
 //  Keep in mind that sometimes there might be a sensor error.
@@ -33,3 +33,31 @@ const calcTempAmplitude = function (temps) {
 };
 const amplitude = calcTempAmplitude(temperature);
 console.log(amplitude);
+
+// PROBLEM 2:
+// Function should now receive 2 arrays of temps
+
+// 1) Understanding the problem
+// - With 2 arrays, should we implement functionality twice? NO! Just merge two arrays
+
+// 2) Breaking up into sub-problems
+// - Merge 2 arrays
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeNew = calcTempAmplitudeNew([2, 4, 5, 8], [2, 4, 5, 6]);
+console.log(amplitudeNew);
