929cf09f1617fc70ecc3b086064954fe2aa36e38
Samir Jadhav <samirzjadhav@gmail.com>
2022-08-01 14:55:37 +0530

    feat: starting condition

--- 04-javascript-in-the-browser/pig-game/script.js
+++ 04-javascript-in-the-browser/pig-game/script.js
@@ -7,3 +7,8 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+//Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
