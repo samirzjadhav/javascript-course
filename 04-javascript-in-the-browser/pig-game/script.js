67369754925bf4e538d804ca3a2d2222e0e881fe
Samir Jadhav <samirzjadhav@gmail.com>
2022-08-01 14:53:59 +0530

    feat: selecting elements

--- 04-javascript-in-the-browser/pig-game/script.js
+++ 04-javascript-in-the-browser/pig-game/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+//Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
