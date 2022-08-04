ce0b0d9e5d433624a51b2d7c3317ddd790cacf92
Samir Jadhav <samirzjadhav@gmail.com>
2022-08-04 22:17:11 +0530

    misc: hoisting and TDZ

--- 05-behind-the-scenes/script.js
+++ 05-behind-the-scenes/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 function calAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -32,3 +33,48 @@ function calAge(birthYear) {
 
 const firstName = 'jonas';
 calAge(1991);
+*/
+
+//Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'samir';
+let job = 'student';
+const year = 2004;
+
+// function
+console.log(addDecl(2, 4));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts);
+
+if (!numProducts) deleteShopingCart();
+
+var numProducts = 10;
+
+function deleteShopingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
