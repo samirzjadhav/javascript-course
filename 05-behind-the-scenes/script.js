467728c2e18789d85e051dfc74785d187da28a55
Samir Jadhav <samirzjadhav@gmail.com>
2022-08-04 20:53:11 +0530

    misc: scoping in practic

--- 05-behind-the-scenes/script.js
+++ 05-behind-the-scenes/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function calAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+
+  if (birthYear >= 1981 && birthYear < 1996) {
+    var millienial = true;
+    //Creating NEW variable with same name as outer scope's variable
+    const firstName = 'samir';
+
+    //Reassigning outer scope's variable
+    //output = 'new output!';
+
+    const str = `Oh, and you're millinial, ${firstName}`;
+    console.log(str);
+
+    function add(a, b) {
+      return a + b;
+    }
+  }
+
+  console.log(millienial);
+
+  return age;
+}
+
+const firstName = 'jonas';
+calAge(1991);
