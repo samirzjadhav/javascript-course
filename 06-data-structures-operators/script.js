17d78d6588c350bcb1ba20aac69f4efa21371589
Samir Jadhav <samirzjadhav@gmail.com>
2022-08-08 17:08:32 +0530

    misc: switching variable and receiveing value from function

--- 06-data-structures-operators/script.js
+++ 06-data-structures-operators/script.js
@@ -11,6 +11,9 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
   openingHours: {
     thu: {
@@ -39,5 +42,19 @@ console.log([x, y, z]);
 const [first, , second] = restaurant.categories;
 console.log(first, second);
 
-const [main, , secondary] = restaurant.categories;
+let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
+
+//Switching Variable
+
+const temp = main;
+main = secondary;
+secondary = temp;
+console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Receive 2 return value from a function
+const [starter, mainCourse] = restaurant.order(1, 2);
+console.log(starter, mainCourse);
