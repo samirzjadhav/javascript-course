183af9c02c98f0d00a4f8e6681ab77237b308f0c
Samir Jadhav <samirzjadhav@gmail.com>
2022-08-19 17:26:34 +0530

    misc: method

--- 06-data-structures-operators/script.js
+++ 06-data-structures-operators/script.js
@@ -103,9 +103,13 @@ console.log(restaurant.openingHours?.fri?.open);
 
 const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 for (const day of days) {
-  const open = restaurant.openingHours[day]?.open;
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
   console.log(`On ${day}, we open at ${open}`);
 }
+
+// Method
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRotten?.(0, 1) ?? 'Method does not exist');
 
 /*
 //Looping arrays: the for-of loop
