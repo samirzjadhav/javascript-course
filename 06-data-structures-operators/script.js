cedd157531c7f6a078e3a8a7e4e427c7f467be67
Samir Jadhav <samirzjadhav@gmail.com>
2022-08-22 22:29:46 +0530

    misc: map fundamental

--- 06-data-structures-operators/script.js
+++ 06-data-structures-operators/script.js
@@ -94,6 +94,38 @@ const restaurant = {
   },
 };
 
+//Maps Fundamentals
+const rest = new Map();
+rest.set('name', 'Classica Italioo');
+rest.set(1, 'Firenze, Italy');
+rest.set(2, 'Lisbon, Portugal');
+
+console.log(rest.get('name'));
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'we are open :D')
+  .set(false, 'we are close :(');
+
+console.log(rest.get('open'));
+console.log(rest.get('categories'));
+
+const time = 10;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+//rest.clear();
+console.log(rest.size);
+
+const arr = [1, 2];
+rest.set(arr, 'test');
+console.log(rest.get(arr));
+console.log(document.querySelector('h1'), 'heading');
+console.log(rest);
+
+/*
 const ordersSet = new Set([
   'pasta',
   'pizza',
@@ -126,7 +158,6 @@ console.log(new Set(['waiter', 'manager', 'chef', 'waiter', 'chef']).size);
 
 console.log(new Set('samirjadhaw').size);
 
-/*
 console.log(`we are open on ${properties}`);
 
 for (const day of Object.keys(openingHours)) {
