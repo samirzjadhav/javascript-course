86b4293db8998c18cb31e0442424e98cdf2340c8
Samir Jadhav <samirzjadhav@gmail.com>
2022-08-28 20:43:30 +0530

    misc: flight delayed example

--- 06-data-structures-operators/script.js
+++ 06-data-structures-operators/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 // const openingHours = {
 //   thu: {
 //     open: 12,
@@ -94,6 +90,28 @@ const restaurant = {
   },
 };
 
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+for (const flight of flights.split('+')) {
+  const [type, form, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} ${getCode(form)} ${getCode(to).join} (${time.replace(':', 'h')})`.padStart(
+    46
+  );
+  console.log(output);
+}
+
+/*
 // Working with string - part - 3
 console.log('a+very+nice+string'.split('+'));
 console.log('Samir Jadhaw'.split(' '));
@@ -143,7 +161,6 @@ const planeInLine = function (n) {
 planeInLine(5);
 planeInLine(3);
 
-/*
 // Working with string - part - 2
 const airline = 'TAP Air Portugal';
 console.log(airline.toLocaleLowerCase());
