b87ec326882f76e77b9c687c0685ecc31f364a46
Samir Jadhav <samirzjadhav@gmail.com>
2022-08-31 22:07:11 +0530

    misc: value vs reference

--- 07-functions/script.js
+++ 07-functions/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 const bookings = [];
 
 const createBookings = function (
@@ -25,3 +26,34 @@ createBookings('HW123', undefined, 123);
 createBookings('HT858', 12, 500);
 createBookings('POKE', 34, 1000);
 createBookings('HER45', 54, undefined);
+*/
+
+const flight = 'LH234';
+const samir = {
+  name: 'Samir Jadhaw',
+  passport: 345435537463,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 345435537466) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+//this is same as doing...
+
+// checkIn(flight, samir);
+// console.log(flight);
+// console.log(samir);
+
+const newPassport = function (person) {
+  person.passenger = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(samir);
+checkIn(flight, samir);
