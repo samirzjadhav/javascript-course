859c728153ae18376da3d186d43f4bcaf607e0e7
Samir Jadhav <samirzjadhav@gmail.com>
2022-09-12 00:34:59 -0700

    misc: maximum value in reduce method

--- 08-arrays-bankist/script.js
+++ 08-arrays-bankist/script.js
@@ -250,3 +250,10 @@ console.log(balance);
 let balance2 = 0;
 for (const mov of movements) balance2 += mov;
 console.log(balance2);
+
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
