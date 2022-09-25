a9c51b023e51f1dd01115f5947d3e4a233815970
Samir Jadhav <samirzjadhav@gmail.com>
2022-09-25 18:24:41 +0530

    arrays method practice

--- 08-arrays-bankist/script.js
+++ 08-arrays-bankist/script.js
@@ -471,7 +471,6 @@ const overAllBalance2 = accounts
   .reduce((acc, mov) => acc + mov, 0);
 
 console.log(overAllBalance2);
-*/
 
 // String
 const owner = ['samir', 'pawan', 'amol', 'yash'];
@@ -540,3 +539,68 @@ labelBalance.addEventListener('click', function () {
   const movementsUI2 = [...document.querySelectorAll('.movements__value')];
   console.log(movementsUI2);
 });
+*/
+
+// ARRAY METHODS PRACTICE
+// 1.
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, cur) => sum + cur, 0);
+console.log(bankDepositSum);
+
+// 2.
+// const numDeposits1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov >= 1000);
+
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  // .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
+  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+console.log(numDeposits1000);
+
+// Prefixed ++ operator
+let a = 10;
+console.log(a++);
+console.log(a);
+
+// 3.
+const sums = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      cur > 0 ? (sums.deposit += cur) : (sums.withdrawals += cur);
+      return sums;
+    },
+    { deposit: 0, withdrawals: 0 }
+  );
+console.log(sums);
+
+const { deposit, withdrawals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      sums[cur > 0 ? 'deposit' : 'withdrawals'] += cur;
+      return sums;
+    },
+    { deposit: 0, withdrawals: 0 }
+  );
+console.log(deposit, withdrawals);
+
+// 4.
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const capitzalize = str => str[0].toUpperCase() + str.slice(1);
+  const expections = ['a', 'an', 'the', 'and', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (expections.includes(word) ? word : capitzalize(word)))
+    .join(' ');
+  return capitzalize(titleCase);
+};
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a long title but not to LONG'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
