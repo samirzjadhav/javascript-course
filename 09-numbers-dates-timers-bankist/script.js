33a8474bf9ecbbce77c7603740b88eedec88e02a
Samir Jadhav <samirzjadhav@gmail.com>
2022-10-16 22:07:17 +0530

    misc: internationalizing dates

--- 09-numbers-dates-timers-bankist/script.js
+++ 09-numbers-dates-timers-bankist/script.js
@@ -81,7 +81,7 @@ const inputClosePin = document.querySelector(".form__input--pin");
 /////////////////////////////////////////////////
 // Functions
 
-const formatMovementDate = function (date) {
+const formatMovementDate = function (date, locale) {
   const calcDaysPassed = (date1, date2) =>
     Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
 
@@ -91,10 +91,13 @@ const formatMovementDate = function (date) {
   if (daysPassed === 0) return "Today";
   if (daysPassed === 1) return "Yesterday";
   if (daysPassed <= 7) return `${daysPassed} days ago`;
-  const day = `${date.getDate()}`.padStart(2, 0);
-  const month = `${date.getMonth() + 1}`.padStart(2, 0);
-  const year = date.getFullYear();
-  return `${day}/${month}/${year} `;
+
+  // const day = `${date.getDate()}`.padStart(2, 0);
+  // const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  // const year = date.getFullYear();
+  // return `${day}/${month}/${year} `;
+
+  return new Intl.DateTimeFormat(locale).format(date);
 };
 
 const displayMovements = function (acc, sort = false) {
@@ -110,7 +113,7 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
     const date = new Date(acc.movementsDates[i]);
-    const displayDate = formatMovementDate(date);
+    const displayDate = formatMovementDate(date, acc.locale);
 
     const html = `
       <div class="movements__row">
@@ -178,6 +181,8 @@ const updateUI = function (acc) {
 ///////////////////////////////////////
 // Event handlers
 let currentAccount;
+
+// Fake always logged in
 currentAccount = account1;
 updateUI(currentAccount);
 containerApp.style.opacity = 100;
@@ -199,13 +204,30 @@ btnLogin.addEventListener("click", function (e) {
     containerApp.style.opacity = 100;
 
     // create current date and time
+    // const now1 = new Date();
+    // const day = `${now1.getDate()}`.padStart(2, 0);
+    // const month = `${now1.getMonth() + 1}`.padStart(2, 0);
+    // const year = now1.getFullYear();
+    // const hour = `${now1.getHours()}`.padStart(2, 0);
+    // const minute = `${now1.getMinutes()}`.padStart(2, 0);
+    // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${minute} `;
+    const now = new Date();
+    const options = {
+      hour: "numeric",
+      minute: "numeric",
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+    };
+
+    const locel = navigator.language;
+    console.log(locel);
+
     const now1 = new Date();
-    const day = `${now1.getDate()}`.padStart(2, 0);
-    const month = `${now1.getMonth() + 1}`.padStart(2, 0);
-    const year = now1.getFullYear();
-    const hour = `${now1.getHours()}`.padStart(2, 0);
-    const minute = `${now1.getMinutes()}`.padStart(2, 0);
-    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${minute} `;
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now1);
 
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = "";
