64cc884cd61c142dd72c72317ff49e9255f73c39
Samir Jadhav <samirzjadhav@gmail.com>
2022-11-04 21:02:28 +0530

    misc: Dom traversing

--- 10-advanced-dom-bankist/script.js
+++ 10-advanced-dom-bankist/script.js
@@ -195,11 +195,44 @@ document.querySelector(".nav__link").addEventListener("click", function (e) {
 
 document.querySelector(".nav__links").addEventListener("click", function (e) {
   this.style.backgroundColor = randomColor();
-  console.log("Container", e.target);
+  console.log("Container", e.target, e.currentTarget);
 });
 
-document.querySelector(".nav").addEventListener("click", function (e) {
-  this.style.backgroundColor = randomColor();
-  console.log("Nav", e.target);
-});
+document.querySelector(".nav").addEventListener(
+  "click",
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log("Nav", e.target, e.currentTarget);
+  },
+  true
+);
 */
+
+// DOM Traversing
+const h1 = document.querySelector("h1");
+
+// Going downwards: childs
+console.log(document.querySelectorAll(".highlight"));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "orange";
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest(".header").style.background = "var(--gradient-secondary)";
+h1.closest("h1").style.background = "var( --color-primary)";
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = "scale(0.5)";
+});
