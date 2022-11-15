0f825541a1c93af36e467c83bd191c921b5cfb4c
Samir Jadhav <samirzjadhav@gmail.com>
2022-11-15 21:42:19 +0530

    misc: prototype

--- 11-object-oriented-programming/script.js
+++ 11-object-oriented-programming/script.js
@@ -17,3 +17,27 @@ const mitilda = new Person("mitilda", 2003);
 const jack = new Person("jack", 2019);
 console.log(mitilda, jack);
 console.log(jonas instanceof Person);
+
+// Prototypes
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2022 - this.birthYear);
+};
+
+const samir = new Person("samir", 2004);
+samir.calcAge();
+jonas.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(mitilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOfLinkedObjects
+Person.prototype.species = "homo species";
+console.log(jonas.species, mitilda.species);
+
+console.log(jonas.hasOwnProperty("firstName"));
+console.log(jonas.hasOwnProperty("birthYear"));
