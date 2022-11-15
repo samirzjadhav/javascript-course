255bae8abec975447a58b247e4a13591a00e0f93
Samir Jadhav <samirzjadhav@gmail.com>
2022-11-15 19:31:59 +0530

    constructon function and new operator

--- 11-object-oriented-programming/script.js
+++ 11-object-oriented-programming/script.js
@@ -1,1 +1,19 @@
-'use strict';
+"use strict";
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const jonas = new Person("jonas", 1990);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const mitilda = new Person("mitilda", 2003);
+const jack = new Person("jack", 2019);
+console.log(mitilda, jack);
+console.log(jonas instanceof Person);
