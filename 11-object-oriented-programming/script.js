08c83df74291656b4c38cbddd0650d50d0a2d6fd
Samir Jadhav <samirzjadhav@gmail.com>
2022-11-16 20:36:18 +0530

    coding challenge 01

--- 11-object-oriented-programming/script.js
+++ 11-object-oriented-programming/script.js
@@ -59,3 +59,43 @@ Array.prototype.unique = function () {
 };
 
 console.log(arr.unique);
+
+/*Coding Challenge #1
+Your tasks:
+1. Use a constructor function to implementa 'Car'.
+ Acarhasa 'make' anda 'speed' property. The 'speed' property
+ is the current speed of the car in km/h
+2. Implementan 'accelerate' method that will increase the 
+car 's speed by 10, and log the new speed to the console
+3. Implementa 'brake' method that will decrease the car 
+ 's speed by 5,and log the new speed to the console
+4. Create2 'Car' objects and experiment with calling 'accelerate'
+and 'brake' multiple times on each of them
+Test data:
+§ Data car 1: 'BMW' going at 120 km/h
+§ Data car 2: 'Mercedes' going at 95 km/h
+GOOD LUCK 😀
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const bmw = new Car("BMW", 120);
+const mercedes = new Car("mercedes", 95);
+
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
+bmw.accelerate();
