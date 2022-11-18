b178aa2b1bdf1d4e71eac828b9d3e069a62e8315
Samir Jadhav <samirzjadhav@gmail.com>
2022-11-18 21:35:22 +0530

    completed challenge 02

--- 11-object-oriented-programming/script.js
+++ 11-object-oriented-programming/script.js
@@ -203,3 +203,48 @@ console.log(steven.__proto__ === PersonProto);
 const samir = Object.create(PersonProto);
 samir.init("samir", 2004);
 samir.CalcAge();
+
+/*Coding Challenge #2
+Your tasks:
+1. Re-create Challenge #1,but this time using an ES6 class (call it 'CarCl')
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide
+by 1.6)
+3. Add a setter called 'speedUS' which sets the current speed in mi/h(but
+converts it to km/h before storing the value, by multiplying the input by 1.6)
+4. Create a new car and experiment with the 'accelerate' and 'brake'
+methods, and with the getter and setter.
+Test data:
+§ Data car 1: 'Ford' going at 120 km/h GOOD LUCK 😀
+*/
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate = function () {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  };
+
+  brake = function () {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  };
+
+  get speedUs() {
+    return this.speed / 1.6;
+  }
+
+  set speedUs(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new CarCl("Ford", 120);
+console.log(ford);
+console.log(ford.speedUs);
+ford.accelerate();
+ford.speedUs = 30;
+console.log(ford);
