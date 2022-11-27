fa296d79f15403cf5219c06449a165a614013ffc
Samir Jadhav <samirzjadhav@gmail.com>
2022-11-27 18:52:10 +0530

    feat: added getMovements call function

--- 11-object-oriented-programming/script.js
+++ 11-object-oriented-programming/script.js
@@ -402,27 +402,32 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    // Protected Property
+    this._pin = pin;
+    this._movements = [];
     this.locale = navigator.language;
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
   // Public interface
+  getMovements() {
+    return this._movements;
+  }
+
   deposite(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposite(-val);
   }
 
-  approveLone(val) {
+  _approveLone(val) {
     return true;
   }
 
   requenstLoan(val) {
-    if (this.approveLone(val)) {
+    if (this._approveLone(val)) {
       this.deposite(val);
       console.log(`Loan Approve`);
     }
@@ -435,4 +440,5 @@ const acc1 = new Account("samir", "EUR", 1111);
 acc1.requenstLoan(1000);
 acc1.deposite(250);
 acc1.withdraw(140);
+console.log(acc1.getMovements());
 console.log(acc1);
