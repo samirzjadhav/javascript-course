b2209791e98c56597e745548f983da1e59487afa
Samir Jadhav <samirzjadhav@gmail.com>
2023-01-05 19:34:31 +0530

    my first AJEX call, XMLHttprequest

--- 13-asynchronous/script.js
+++ 13-asynchronous/script.js
@@ -1,6 +1,49 @@
-'use strict';
+"use strict";
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
-///////////////////////////////////////
+const formatNumber = function (num) {
+  var units = ["M", "B", "T", "Q"];
+  var unit = Math.floor((num / 1.0e1).toFixed(0).toString().length);
+  var r = unit % 3;
+  var x = Math.abs(Number(num)) / Number("1.0e+" + (unit - r)).toFixed(2);
+  return x.toFixed(2) + units[Math.floor(unit / 3) - 2];
+};
+
+//
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    window.data = data;
+
+    const currency = Object.keys(data.currencies)[0];
+    const language = Object.values(data.languages)[0];
+
+    const html = `     
+<article class="country">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${formatNumber(
+      data.population
+    )} People</p>
+    <p class="country__row"><span>🗣️</span>${language}</p>
+    <p class="country__row"><span>💰</span>${
+      data.currencies[currency].symbol
+    } ${data.currencies[currency].name}</p>
+  </div>
+</article>`;
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData("india");
+getCountryData("USA");
+getCountryData("portugal");
