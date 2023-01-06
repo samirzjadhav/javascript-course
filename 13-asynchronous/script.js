a1529443d885e04f78eb1c82a8cbd087702533fb
Samir Jadhav <samirzjadhav@gmail.com>
2023-01-06 14:17:00 +0530

    misc: learned about callback hell and  create getCountryAndNeighbour script

--- 13-asynchronous/script.js
+++ 13-asynchronous/script.js
@@ -11,7 +11,7 @@ const formatNumber = function (num) {
   return x.toFixed(2) + units[Math.floor(unit / 3) - 2];
 };
 
-//
+/*
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
@@ -47,3 +47,72 @@ const getCountryData = function (country) {
 getCountryData("india");
 getCountryData("USA");
 getCountryData("portugal");
+*/
+
+const renderContry = function (data, className = "") {
+  const currency = Object.keys(data.currencies)[0];
+  const language = Object.values(data.languages)[0];
+  const html = `     
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${formatNumber(
+        data.population
+      )} People</p>
+      <p class="country__row"><span>🗣️</span>${language}</p>
+      <p class="country__row"><span>💰</span>${
+        data.currencies[currency].symbol
+      } ${data.currencies[currency].name}</p>
+    </div>
+  </article>`;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX CALL COUNTRY 1
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    window.data = data;
+
+    // Render country 1
+    renderContry(data);
+
+    // get neighbour country 2
+    const [neighbour] = data.borders;
+    if (!neighbour) return;
+
+    // AJAX CALL COUNTRY 2
+    const request2 = new XMLHttpRequest();
+    request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener("load", function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+      renderContry(data2, "neighbour");
+    });
+  });
+};
+
+getCountryAndNeighbour("india");
+
+// CallBack Hell
+setTimeout(() => {
+  console.log("1 second passed");
+  setTimeout(() => {
+    console.log("2 second passed");
+    setTimeout(() => {
+      console.log("3 second passed");
+      setTimeout(() => {
+        console.log("4 second passed");
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
