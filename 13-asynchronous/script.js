c29a39d27a68b6b2c0505982a0d7956321877ed7
Samir Jadhav <samirzjadhav@gmail.com>
2023-01-08 20:18:31 +0530

    coding challenges 1

--- 13-asynchronous/script.js
+++ 13-asynchronous/script.js
@@ -172,7 +172,7 @@ const getJSON = function (url, errorMsg = "Something went wrong") {
 //       // Country 2
 //       return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
 //     })
-//     .then((response) => response.json())
+//     .then((response) =>{if(!response.ok)throw new Error(`Country not found (${response.status})`)} response.json())
 //     .then((data) => renderContry(data, "neighbour"))
 //     .catch((err) => {
 //       console.error(`${err}💥💥💥💥`);
@@ -183,30 +183,87 @@ const getJSON = function (url, errorMsg = "Something went wrong") {
 //     });
 // };
 
-const getCountryData = function (country) {
-  // Country 1
-  getJSON(
-    `https://restcountries.com/v3.1/name/${country}`,
-    "country not found "
-  )
-    .then((data) => {
-      renderContry(data[0]);
-      const neighbour = data[0].borders[0];
-      if (!neighbour) throw new Error("no neighbour found!");
+// const getCountryData = function (country) {
+//   // Country 1
+//   getJSON(
+//     `https://restcountries.com/v3.1/name/${country}`,
+//     "country not found "
+//   )
+//     .then((data) => {
+//       renderContry(data[0]);
+//       const neighbour = data[0].borders[0];
+//       if (!neighbour) throw new Error("no neighbour found!");
 
+//       console.log(data);
+//       // Country 2
+//       getJSON(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     })
+//     .then((data) => renderContry(data, "neighbour"))
+//     .catch((err) => {
+//       console.error(`${err}💥💥💥💥`);
+//       renderError(`Something went wrong 💥💥💥 ${err} try again!`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+// btn.addEventListener("click", function () {
+//   getCountryData("india");
+// });
+
+// Coding challenge 1
+/*
+Your tasks:
+PART 1
+1. Create a function 'whereAmI' which takes asn inputs a latitude value 
+('lat') and a longitude value ('lng') (these are GPS coordinates, examples 
+are in test data below).
+2. Do “reverse geocoding” of the provided coordinates. Reverse geocoding means
+to convert coordinates to a meaningful location, like a city and country name.
+Use this API to do reverse geocoding: https://geocode.xyz/api. The AJAX call 
+will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json.
+Use the fetch API and promises to get the data. Do not use the 'getJSON' function 
+we created, that is cheating 😉
+3. Once you have the data,take a look at it in the console to see all the attributes 
+that you received about the provided location. Then, using this data, log a message 
+like this to the console: “You are in Berlin, Germany”
+4. Chain a.catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second.If youre loadfast,you will 
+get this error with code 403. This is an error with the request. Remember, fetch() 
+does not reject the promise in this case. So create an error to reject the promise 
+yourself, with a meaningful error message
+PART 2
+6. Now it's time to use the received data to render a country.So take the relevant 
+attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors,just like we haved one in the last lecture 
+(you can even copy this code, no need to type the same code)
+      The Complete JavaScript Course 30
+Test data:
+§ Coordinates 1: 52.508, 13.381 (Latitude, Longitude) 
+§ Coordinates 2: 19.037, 72.873
+§ Coordinates 3: -33.933, 18.474
+GOOD LUCK 😀
+*/
+
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then((res) => {
+      if (!res.ok) throw new Error(`problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then((data) => {
       console.log(data);
-      // Country 2
-      getJSON(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+      console.log(`your in ${data.city},${data.country}`);
+      return fetch(`https://restcountries.com/v3.1/name/${data.country}`);
     })
-    .then((data) => renderContry(data, "neighbour"))
-    .catch((err) => {
-      console.error(`${err}💥💥💥💥`);
-      renderError(`Something went wrong 💥💥💥 ${err} try again!`);
+    .then((res) => {
+      console.log(res);
+      if (!res.ok) throw new Error(`country not found (${res.status})`);
+      return res.json();
     })
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
-    });
+    .then((data) => renderContry(data[0]))
+    .catch((err) => console.error(`${err.massage} 💥 `));
 };
-btn.addEventListener("click", function () {
-  getCountryData("india");
-});
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
