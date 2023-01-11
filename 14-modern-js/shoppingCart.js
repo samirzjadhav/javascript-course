a9db299586e406b962d6de0c597d0d78f8c13749
Samir Jadhav <samirzjadhav@gmail.com>
2023-01-11 19:42:55 +0530

    misc: added shoppingCart.js

---
+++ 14-modern-js/shoppingCart.js
@@ -0,0 +1,16 @@
+// Exporting module
+console.log("Exporting module");
+
+const shoppingCost = 10;
+const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+console.log(addToCart);
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as tq };
