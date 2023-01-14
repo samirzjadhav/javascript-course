e2628bfff7aaab0e7e5ab7f0facc1b6682fd22d7
Samir Jadhav <samirzjadhav@gmail.com>
2023-01-14 17:46:50 +0530

    feat: init parcel and transformer-sass

--- 15-forkify/src/js/controller.js
+++ 15-forkify/src/js/controller.js
@@ -1,4 +1,4 @@
-const recipeContainer = document.querySelector('.recipe');
+const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +11,30 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipes = async function () {
+  try {
+    const res = await fetch(
+      "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcb37"
+      //  "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    console.log(res, data);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+showRecipes();
