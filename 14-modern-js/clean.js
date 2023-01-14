cde928acbc8089cbbf66d0f58b494a62c037d90c
Samir Jadhav <samirzjadhav@gmail.com>
2023-01-14 15:06:22 +0530

    misc: fix some bad code part 1

--- 14-modern-js/clean.js
+++ 14-modern-js/clean.js
@@ -1,26 +1,26 @@
-var budget = [
-  { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
-  { value: -45, description: 'Groceries 🥑', user: 'jonas' },
-  { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
-  { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
-  { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
-  { value: -20, description: 'Candy 🍭', user: 'matilda' },
-  { value: -125, description: 'Toys 🚂', user: 'matilda' },
-  { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+const budget = [
+  { value: 250, description: "Sold old TV 📺", user: "jonas" },
+  { value: -45, description: "Groceries 🥑", user: "jonas" },
+  { value: 3500, description: "Monthly salary 👩‍💻", user: "jonas" },
+  { value: 300, description: "Freelancing 👩‍💻", user: "jonas" },
+  { value: -1100, description: "New iPhone 📱", user: "jonas" },
+  { value: -20, description: "Candy 🍭", user: "matilda" },
+  { value: -125, description: "Toys 🚂", user: "matilda" },
+  { value: -1800, description: "New Laptop 💻", user: "jonas" },
 ];
 
-var limits = {
+const spendingLimit = {
   jonas: 1500,
   matilda: 100,
 };
 
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
+const addExpense = function (value, description, user) {
+  if (!user) user = "jonas";
   user = user.toLowerCase();
 
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
+  let lim;
+  if (spendingLimit[user]) {
+    lim = spendingLimit[user];
   } else {
     lim = 0;
   }
@@ -29,22 +29,22 @@ var add = function (value, description, user) {
     budget.push({ value: -value, description: description, user: user });
   }
 };
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
+addExpense(10, "Pizza 🍕");
+addExpense(100, "Going to movies 🍿", "Matilda");
+addExpense(200, "Stuff", "Jay");
 console.log(budget);
 
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
+const check = function () {
+  for (const el of budget) {
+    let lim;
+    if (spendingLimit[el.user]) {
+      lim = spendingLimit[el.user];
     } else {
       lim = 0;
     }
 
     if (el.value < -lim) {
-      el.flag = 'limit';
+      el.flag = "limit";
     }
   }
 };
@@ -52,13 +52,15 @@ check();
 
 console.log(budget);
 
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
+const bigExpenses = function (limit) {
+  let output = "";
+  for (const el of budget) {
     if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
+      output += el.description.slice(-2) + " / "; // Emojis are 2 chars
     }
   }
   output = output.slice(0, -2); // Remove last '/ '
   console.log(output);
 };
+
+bigExpenses(1000);
